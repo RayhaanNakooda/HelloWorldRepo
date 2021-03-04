febb8c16c75315fb521b62b23766f48ac02fbd03
Rayhaan Nakooda <rayhaannak1@gmail.com>
2021-03-04 11:44:30 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Suva!!!"); 
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
